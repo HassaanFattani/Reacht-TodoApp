9a0913f636eca22cd1403550cbbc8a3519bcbc91
HassaanFattani <hassaan_fattani@hotmail.com>
2019-09-12 23:33:51 +0500

    Revert "Merge pull request #2 from HassaanFattani/Feature/App"
    This reverts commit e1afd2261ad79cd3dd9981f2eb4edb860007869c, reversing
    changes made to bee66c78f968df098b9b2cf4af677b09f3bccfb9.
    
    Changes in Revert

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         My First Class
       </header>
     </div>
